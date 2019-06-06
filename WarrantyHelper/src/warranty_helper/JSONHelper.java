21c76f674bb42b353f64cda14c740c76471ff9b1
Levi <levimogford@gmail.com>
2019-06-06 04:29:07 -0600

    Add files via upload

---
+++ WarrantyHelper/src/warranty_helper/JSONHelper.java
@@ -0,0 +1,13 @@
+package warranty_helper;
+
+public class JSONHelper {
+	/*final String json = "{\"results\":[{\"rowIndex\":1,\"WO#\":\"1\",\"x\":\"a\"},{\"rowIndex\":2,\"WO#\":\"2\",\"x\":\"b\"},{\"rowIndex\":3,\"WO#\":\"3\",\"x\":\"c\"},{\"rowIndex\":4,\"WO#\":\"4\"},{\"rowIndex\":5,\"WO#\":\"5\"},{\"rowIndex\":6,\"WO#\":\"6\"},{\"rowIndex\":7,\"WO#\":\"7\"},{\"rowIndex\":8,\"WO#\":\"8\"},{\"rowIndex\":9,\"WO#\":\"9\"}]}";
+	
+	public static String getSheet(String sheetName, String sheetID) {
+		UserAgent ua = new UserAgent();
+		String sheetURL = "https://api.sheetson.com/v1/sheets/:sheetName?spreadsheetId=:spreadsheetId";
+		
+		
+		userAgent.visit(sheetURL);
+	}*/
+}
